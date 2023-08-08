9346cbb8d261d7d36ee8af1d8f5827cd1b19bf93
nazrilganteng17 <frizalfadri1705@gmail.com>
2023-08-08 12:49:51 +0700

    latihan-modules

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
+<script>
+import Navbar from './components/Navbar.vue';
+</script>
 <template>
   <div id="nav">
+    <Navbar />
     <router-link to="/">Home</router-link> |
+    <router-link to="/users">User</router-link> 
+    | <router-link to="/berita">Berita</router-link> 
   </div>
   <router-view />
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,29 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Home from "../views/Home.vue";
+import User from "../views/User.vue";
+import Berita from "../views/Berita.vue";
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: "/users",
+        name: "User",
+        component: User,
+    },
+    {
+      path: "/berita",
+      name: "Berita",
+      component: Berita,
   },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    history: createWebHistory(),
+    routes,
 });
 
 export default router;
